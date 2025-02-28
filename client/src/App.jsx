9b3f5cfc7abcbbdc3646113af4058199c3b7dfc5
Thibaud Provost <thibaudprovost50@gmail.com>
2025-02-28 15:09:54 +0100

    conflit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,8 @@ import { Restricted } from "./pages/Restricted";
 import { Login } from "./pages/Login";
 import { Accueil } from "./pages/Accueil/Accueil.jsx";
 import { HomeStagiaire } from "./pages/Home-stagiaire/Home-stagiaire.jsx";
-<<<<<<< HEAD
 import { AccueilStagiaire } from "./pages/Stagiaire/AccueilStagiaire";
-=======
 
->>>>>>> 82a5ece (home-stagiaire)
 function App() {
   const [authenticated, setAuthenticated] = useState(true);
 
@@ -18,8 +15,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Restricted authenticated={authenticated} setAuthenticated={setAuthenticated} />} />
         <Route path="/connexion" element={<Login setAuthenticated={setAuthenticated} />} />
-        <Route path="/accueil" element={<Accueil/>} />
-        <Route path="/homestagiaire" element={<HomeStagiaire/>} />
+        <Route path="/accueil" element={<Accueil />} />
+        <Route path="/homestagiaire" element={<HomeStagiaire />} />
         <Route path="/accueil-stagiaire" element={<AccueilStagiaire />} />
       </Routes>
     </BrowserRouter>
