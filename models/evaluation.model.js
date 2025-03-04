1f4bcbe93d8270f9011405e4361472a799e1b4d6
Carl Richard <carl.richard@institutsolacroup.com>
2025-03-04 13:58:00 +0100

    fix

--- models/evaluation.model.js
+++ models/evaluation.model.js
@@ -2,66 +2,49 @@ import { DataTypes } from "sequelize";
 import { sequelize } from "../database.js";
 import Utilisateur from "./utilisateur.model.js";
 import Reponse from "./reponse.model.js";
-import Questionnaire from "./questionnaire.model.js"; // Import du modèle Questionnaire
+import Questionnaire from "./questionnaire.model.js"; 
 
-const Evaluation = sequelize.define("Evaluation", {
+const Evaluation = sequelize.define('Evaluation', {
   id: {
     type: DataTypes.INTEGER,
-    autoIncrement: true,
     primaryKey: true,
+    autoIncrement: true
   },
   id_evaluateur: {
     type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: Utilisateur,
-      key: "id",
-    },
-    onDelete: "CASCADE",
+    allowNull: false
   },
   id_reponse: {
     type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: Reponse,
-      key: "id",
-    },
-    onDelete: "CASCADE",
-  },
-  // Ajouter le champ id_questionnaire pour la relation avec le questionnaire
-  id_questionnaire: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: Questionnaire,
-      key: "id",
-    },
-    onDelete: "CASCADE",
+    allowNull: false
   },
   note_formateur: {
     type: DataTypes.INTEGER,
-    allowNull: false,
-    validate: {
-      min: 0,
-      max: 16,
-    },
+    allowNull: false
   },
   commentaire: {
-    type: DataTypes.TEXT,
-    allowNull: false,
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  id_questionnaire: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  id_evalue: {  
+    type: DataTypes.INTEGER,
+    allowNull: true
   },
 }, {
   tableName: "evaluation",
   timestamps: true,
 });
 
-
 // Définition des relations
 Evaluation.belongsTo(Utilisateur, { foreignKey: "id_evaluateur", as: "evaluateur" });
 Evaluation.belongsTo(Reponse, { foreignKey: "id_reponse", as: "reponse" });
 Evaluation.belongsTo(Questionnaire, { foreignKey: "id_questionnaire", as: "questionnaire" }); // Nouvelle relation
 // Ajoutez ceci dans votre modèle Evaluation
-Evaluation.belongsTo(Utilisateur, { foreignKey: "id_utilisateur", as: "evalue" });
+Evaluation.belongsTo(Utilisateur, { foreignKey: "id_evalue", as: "evalue" });
 
 
-export default Evaluation;
+export default Evaluation; 
