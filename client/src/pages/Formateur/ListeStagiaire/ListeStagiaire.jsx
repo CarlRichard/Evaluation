bbb9418e155963501834d40d47e397d61dbc2eb5
Thibaud Provost <thibaudprovost50@gmail.com>
2025-03-05 12:22:48 +0100

    fetch

--- client/src/pages/Formateur/ListeStagiaire/ListeStagiaire.jsx
+++ client/src/pages/Formateur/ListeStagiaire/ListeStagiaire.jsx
@@ -1,50 +1,31 @@
-import { useEffect, useState } from "react";
-import { NavFormateur } from "../../../Components/Navs/NavFormateur";
+import { useEffect } from "react"
+import { NavFormateur } from "../../../Components/Navs/NavFormateur"
+
 
 export const ListeStagiaire = () => {
-    const [stagiaires, setStagiaires] = useState([]); 
-    const [error, setError] = useState(null); // gestion erreur
 
-    const token = localStorage.getItem("token");
+    let tokenUser = localStorage.getItem("token");
 
     useEffect(() => {
-        if (!token) return; 
-
         fetch('http://localhost:3000/api/utilisateur', {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
-                Authorization: `Bearer ${token}`,
+                Authorization: `Bearer ${tokenUser}`,
             },
         })
-            .then(res => res.json()) 
-            .then(data => {
-                console.log(data)
-                if (Array.isArray(data.utilisateurs)) {
-                    setStagiaires(data.utilisateurs); 
-                } else {
-                    setError("erreur de la requete.");
-                }
-            })
-            .catch(error => setError("Erreur réseau : " + error.message)); 
-    }, [token]);
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(console.error)
+    })
 
     return (
         <>
             <NavFormateur />
             <h1>Stagiaires</h1>
 
-            {error && <p>Erreur: {error}</p>} {/* Affiche l'erreur si elle existe */}
 
-            {Array.isArray(stagiaires) && stagiaires.length === 0 ? (
-                <p>Aucun stagiaire trouvé.</p>
-            ) : (
-                <ul>
-                    {Array.isArray(stagiaires) && stagiaires.map((stagiaire) => (
-                        <li key={stagiaire.id}>{stagiaire.nom} {stagiaire.prenom}</li> 
-                    ))}
-                </ul>
-            )}
         </>
-    );
-};
+    )
+}
+
