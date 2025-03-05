5af058f660915a77fd402e0a812dfa7a25ab5cb4
Carl Richard <carl.richard@institutsolacroup.com>
2025-03-05 10:01:27 +0100

    fix back: role et formation par default

--- models/utilisateur.model.js
+++ models/utilisateur.model.js
@@ -27,13 +27,15 @@ const Utilisateur = sequelize.define('utilisateur', {
   role: {
     type: DataTypes.INTEGER,
     allowNull: false,
+    defaultValue: 0,
     validate: {
       isIn: [[0, 1]], // 0 = etudiant ou 1 = formateur
     }
   },
   formation: {
-    type: DataTypes.ENUM('Back-End', 'Front-End', 'CDA', 'PrépaNum', 'PrépaNum2', 'POA'),
-    allowNull: false
+    type: DataTypes.ENUM('Back-End', 'Front-End', 'CDA', 'PrépaNum', 'PrépaNum2', 'POA', ''),
+    defaultValue: '',
+    allowNull: true
   },
   mot_de_passe: {
     type: DataTypes.STRING,
