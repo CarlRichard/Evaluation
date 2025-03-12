eee70c96ea18fbbdeeaa3d16dc35545b0a6d92b6
Carl Richard <carl.richard@institutsolacroup.com>
2025-03-12 12:02:20 +0100

    fix : fichier supprimé inutile

--- routes/stat.route.js
+++
@@ -1,7 +0,0 @@
-
-// Route pour récupérer les statistiques
-router.get('/api/stats/:id_utilisateur', async (req, res) => {
-    const id_utilisateur = req.params.id_utilisateur;
-    const stats = await getStats(id_utilisateur);
-    res.json(stats);
-});
